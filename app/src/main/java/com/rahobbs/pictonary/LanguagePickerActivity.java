7c250eadc06decf18b5ba3b13df7ffae7b79d630
rachael <hobbs.rachael@gmail.com>
2016-11-11 12:05:21 -0500

    added item click listener to language picker list

--- app/src/main/java/com/rahobbs/pictonary/LanguagePickerActivity.java
+++ app/src/main/java/com/rahobbs/pictonary/LanguagePickerActivity.java
@@ -1,11 +1,15 @@
 package com.rahobbs.pictonary;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class LanguagePickerActivity extends AppCompatActivity {
 
@@ -13,10 +17,19 @@ public class LanguagePickerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_language_picker);
+        ListView listView = (ListView)findViewById(R.id.langsList);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> av, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), "myPos "+i, Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
 
