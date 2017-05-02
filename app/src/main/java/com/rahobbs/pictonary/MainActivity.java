22ede2ff4050d49f310976d2f6d0bf5faf0a02ca
rahobbs <hobbs.rachael@gmail.com>
2017-05-02 14:32:16 -0400

    Styling on spinner, shutter icon launches camera

--- app/src/main/java/com/rahobbs/pictonary/MainActivity.java
+++ app/src/main/java/com/rahobbs/pictonary/MainActivity.java
@@ -63,8 +63,12 @@ import com.google.cloud.translate.Translation;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import static com.rahobbs.pictonary.Constants.API_KEY;
@@ -100,6 +104,15 @@ public class MainActivity extends AppCompatActivity {
 
         mImageDetails = (TextView) findViewById(R.id.image_details);
         mShutterButton = (ImageView) findViewById(R.id.shutter_button);
+
+        mShutterButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startCamera();
+                    }
+                }
+        );
     }
 
     private void setupCameraPreview() {
@@ -114,15 +127,15 @@ public class MainActivity extends AppCompatActivity {
 
         preview.addView(mPreview);
 
-        preview.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // get an image from the camera
-                        startCamera();
-                    }
-                }
-        );
+//        preview.setOnClickListener(
+//                new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        // get an image from the camera
+//                        startCamera();
+//                    }
+//                }
+//        );
     }
 
     private void setupFAB() {
@@ -168,7 +181,7 @@ public class MainActivity extends AppCompatActivity {
         });
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.lang_array, android.R.layout.simple_spinner_item);
+                R.array.lang_array, R.layout.language_spinner);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
@@ -193,11 +206,53 @@ public class MainActivity extends AppCompatActivity {
                 this,
                 CAMERA_PERMISSIONS_REQUEST,
                 Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 Manifest.permission.CAMERA)) {
+
+            //TODO: Get taking picture from preview working
+//            mCamera.takePicture(null, null, new Camera.PictureCallback() {
+//                @Override
+//                public void onPictureTaken(byte[] data, Camera camera) {
+//                    File pictureFile = getOutputMediaFile();
+//                    if (pictureFile == null) {
+//                        return;
+//                    }
+//                    try {
+//                        FileOutputStream fos = new FileOutputStream(pictureFile);
+//                        fos.write(data);
+//                        fos.close();
+//                    } catch (FileNotFoundException e) {
+//
+//                    } catch (IOException e) {
+//                    }
+//                }
+//            });
+            //TODO: Only send an image capture intent when gallery button is pushed
             Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(getCameraFile()));
             startActivityForResult(intent, CAMERA_IMAGE_REQUEST);
         }
+    }
+
+    private static File getOutputMediaFile() {
+        File mediaStorageDir = new File(
+                Environment
+                        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                "Rosetta_Lens");
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.d("Rosetta_Lens", "failed to create directory");
+                return null;
+            }
+        }
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss")
+                .format(new Date());
+        File mediaFile;
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator
+                + "IMG_" + timeStamp + ".jpg");
+
+        return mediaFile;
     }
 
     /**
@@ -229,19 +284,16 @@ public class MainActivity extends AppCompatActivity {
                 uploadImage(data.getData());
             } else if (requestCode == CAMERA_IMAGE_REQUEST && resultCode == RESULT_OK) {
             uploadImage(Uri.fromFile(getCameraFile()));
-            //TODO: show image once it is selected/taken, hide camera preview
+
             mPreview.setVisibility(View.INVISIBLE);
             mImageView = (ImageView) findViewById(R.id.image_view);
             mImageView.setImageBitmap(mBitmap);
 
-            }
+        } else {
+            setupCameraPreview();
+        }
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        setupCameraPreview();
-    }
 
     @Override
     public void onRequestPermissionsResult(
